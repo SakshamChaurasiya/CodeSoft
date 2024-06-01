3ddca29827ab2e9360a5cb78fa3fe7b34ac48046
SakshamChaurasiya <161238694+SakshamChaurasiya@users.noreply.github.com>
2024-06-02 02:05:50 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char cal;
+    float a, b;
+    cout << "Enter the operator(+ - / * ): " << endl;
+    cin >> cal;
+
+    cout << "Enter numbers a and b: " << endl;
+    cin >> a >> b;
+    switch (cal)
+    {
+    case '+':
+        cout << a << " + " << b << " = " << a + b << endl;
+        break;
+
+    case '-':
+        cout << a << " - " << b << " = " << a - b << endl;
+        break;
+
+    case '*':
+        cout << a << " * " << b << " = " << a * b << endl;
+        break;
+
+    case '/':
+
+        cout << a << " / " << b << " = " << a / b << endl;
+        break;
+
+    default:
+        cout << "Syntax error" << endl;
+        break;
+    }
+
+    return 0;
+}
